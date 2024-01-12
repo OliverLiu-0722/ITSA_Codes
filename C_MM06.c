848aeece88dff2ac92866c9b2f1bcc1db55f6380
ReversedString <58733690+ReversedString@users.noreply.github.com>
2024-01-12 11:39:14 +0800

    Create C_MM06.c

---
+++ C_MM06.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define and &&
+#define or ||
+#define round(x) (int)((x) + 0.5)
+
+
+int main(){
+    double a;
+    while(scanf("%lf", &a) != EOF) printf("%.1lf\n", ((double)round(a * a * 10)) / 10);
+}
