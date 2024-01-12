190b67295dbee2241092be7a5babfab486346fc6
ReversedString <58733690+ReversedString@users.noreply.github.com>
2024-01-12 11:33:30 +0800

    Create C_MM02.c

---
+++ C_MM02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define and &&
+#define or ||
+
+
+int main(){
+    double a, b;
+    scanf("%lf%lf", &a, &b);
+    printf("%.1lf\n", (a * b / 2));
+}
