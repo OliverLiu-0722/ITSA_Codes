8f50f47b9a06d9641f9d9b3212a38e4146cdab5f
ReversedString <58733690+ReversedString@users.noreply.github.com>
2024-01-12 11:57:39 +0800

    Create C_MM31.c

---
+++ C_MM31.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define and &&
+#define or ||
+#define min(a, b) ({(a) < (b) ? (a) : (b);})
+#define max(a, b) ({(a) > (b) ? (a) : (b);})
+
+int main(){
+    for(int a, n; scanf("%d", &a) != EOF;)printf("%d\n", 6 * (n = (a - 6) / 12 + 1) * n);
+}
