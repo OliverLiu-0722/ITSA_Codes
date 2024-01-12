122ba2b0ad80eeb522b27bed9468c574a6342586
ReversedString <58733690+ReversedString@users.noreply.github.com>
2024-01-12 11:43:27 +0800

    Create C_MM13.c

---
+++ C_MM13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define and &&
+#define or ||
+
+
+int main(){
+    double a;
+    while(scanf("%lf", &a) != EOF) printf("%d\n", (int)ceil(a / 0.238));
+}
